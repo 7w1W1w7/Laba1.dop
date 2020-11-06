21bc671432a2a3c15699a531cb23eba3290ad815
7w1W1w7 <74020936+7w1W1w7@users.noreply.github.com>
2020-11-06 20:09:20 +0300

    Add files via upload

---
+++ Лаба1+.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int num, base;
+    unsigned char minus = 0;
+    unsigned int digits[64];
+    int count;
+    scanf ( "%d", &num);
+    scanf ( "%d", &base);
+    if (num < 0) {
+        num = -num;
+        minus = 1;
+    }
+    for (count = 0; num != 0; count++) {
+        digits[count] = num % base;
+        num /= base;
+    }
+    if (minus) {
+        printf("- ");
+    }
+    int iter = count - 1;
+    while (iter >= 0) {
+        printf("%d ", digits[iter]);
+        iter--;
+    } return 0;
+}
